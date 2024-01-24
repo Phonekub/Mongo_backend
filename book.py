c8f111d459bcfcb2e5d51510f4cc3840c07a7050
Supaiwt <phone2546.pp@gmail.com>
2024-01-24 20:33:44 +0700

    Welcome

--- book.py
+++ book.py
@@ -8,11 +8,11 @@ client = MongoClient(uri)
 db = client["students"]
 collection = db["std_info"]
 
-# books=[
-#     {"id":1,"title":"Book 1","author":"Author 1"},
-#     {"id":2,"title":"Book 2","author":"Author 2"},
-#     {"id":3,"title":"Book 3","author":"Author 3"}
-# ]
+books=[
+    {"id":1,"title":"Book 1","author":"Author 1"},
+    {"id":2,"title":"Book 2","author":"Author 2"},
+    {"id":3,"title":"Book 3","author":"Author 3"}
+]
 stds=[]
 
 @app.route("/")
